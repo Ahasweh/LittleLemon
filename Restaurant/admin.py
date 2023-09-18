d4f7a9ea4fac4cb72d50da8edeb8f7686adddd84
ahmad <ahasweh@gmail.com>
2023-09-18 19:19:09 +0300

    adding the models and mysqlconnection

--- Restaurant/admin.py
+++ Restaurant/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+from django.contrib import admin 
 
 # Register your models here.
+
+from .models import Menu, Booking
+ 
+class BookingAdmin(admin.ModelAdmin):
+    list_display=['name','no_of_guests','booking_date']
+
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display=['title','price','inventory']
+
+admin.site.register(Menu,MenuAdmin)
+admin.site.register(Booking,BookingAdmin)
